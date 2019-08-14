6524dfc2deb9f46a1d6ac0e67f2692d49bdadd16
shownola <swasieko@gmail.com>
2019-08-14 09:14:43 -0700

    Implement Edit functionality

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -30,6 +30,11 @@ function TodoApp(){
     );
     setTodos(updatedTodos);
   };
+  const editTodo = (todoId, newTask) => {
+    const updatedTodos = todos.map(todo =>
+      todo.id === todoId ? {...todo, task: newTask} : todo);
+      setTodos(updatedTodos);
+  }
   return (
   <Paper
     style={{
@@ -48,7 +53,12 @@ function TodoApp(){
   <Grid container justify="center" style={{ marginTop: '1rem'}}>
   <Grid item xs={11} ms={8} lg={4}>
     <TodoForm addTodo={addTodo}/>
-    <TodoList todos={todos} removeTodo={removeTodo} toggleTodo={toggleTodo} />
+    <TodoList
+      todos={todos}
+      removeTodo={removeTodo}
+      toggleTodo={toggleTodo}
+      editTodo={editTodo}
+    />
   </Grid>
   </Grid>
   </Paper>
